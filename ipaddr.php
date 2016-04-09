4852b63285d3ffdf82836d559e9c7e77e11ee201
tsmolar <tony.smolar@gmail.com>
2016-04-09 19:43:57 -0400

    Update ipaddr.php

--- ipaddr.php
+++ ipaddr.php
@@ -2,9 +2,12 @@
 <!-- find and display IP address of the user who hits this page -->
 
 <?php
- $myip=exec('ifconfig eth0 | grep "inet addr" | cut -d: -f2 | cut -d " " -f 1');
 
-  echo "Your IP address is $myip";
+  // Find IP by querying the _SERVER array
+  $myip=$_SERVER['REMOTE_ADDR'];
 
+  echo "<center>Your IP address is $myip</center>";
 ?>
 
+
+</html>
