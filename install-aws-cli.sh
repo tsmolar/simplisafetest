d8a4b5d9dee38c63ca7f3f5a89b35e1a7bf17553
tsmolar <tony.smolar@gmail.com>
2016-04-09 20:18:54 -0400

    Create install-aws-cli.sh

---
+++ install-aws-cli.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+clear
+
+echo "Important!  this script must be run with sudo/root access; It is needed to make changes to /usr/local"
+echo ""
+echo "AWS command line tool will be downloaded and install to /usr/local/aws and /usr/local/bin"
+echo ""
+echo "You will need about 700mb free for temporary use to download this package, and 40mb free on /usr/local"
+echo "To install it"
+echo ""
+echo "To uninstall, remove /usr/local/aws and /usr/local/bin/aws"
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Are you sure you want to install aws tools? (y/N)"
+read x
+
+if [ "$(echo $x | tr [a-z] [A-Z])" != "Y" ]; then
+  exit
+fi
+
+
+echo ""
+echo "Downloading awscli-bundle.zip"
+echo "-----------------------------"
+
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+if [ $? -ne 0 ]; then echo "Error downloading"; exit 1; fi
+
+echo ""
+echo "Extracting awscli-bundle.zip"
+echo "----------------------------"
+
+unzip awscli-bundle.zip
+if [ $? -ne 0 ]; then echo "Error extracting"; exit 1; fi
+
+echo ""
+echo "Installing awscli-bundle"
+echo "------------------------"
+
+./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+if [ $? -ne 0 ]; then echo "Error installing"; exit 1; fi
+
+echo ""
+echo "Cleaning up installation files"
+echo "------------------------------"
+rm -rf awscli-bundle
+rm awscli-bundle.zip
